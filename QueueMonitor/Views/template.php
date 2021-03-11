cf68dfc2d12bf1f7e41996a8437532f3853c825e
root <root@issabel.ridetel.com>
2021-03-11 16:19:18 -0500

    Segundo commit

--- QueueMonitor/Views/template.php
+++ QueueMonitor/Views/template.php
@@ -1,6 +1,6 @@
 <?php namespace Views;
 Use Controllers\colasController as ListaColas;
-if(array_key_exists('error',$datos)){	
+if(array_key_exists('error',$datos)){
 	$page = $_SERVER['PHP_SELF'];
 	$sec = "3";
 	$protocol = ( $_SERVER['HTTPS'] == 'on' ) ? 'https://':'http://';	
@@ -21,44 +21,28 @@ class Template{
     <meta charset="utf-8">
     <title>Asterisk | Monitor ACD </title>
     <link rel="stylesheet" href="<?php echo URL; ?>Views/template/css/bootstrap.min.css" media="screen">
-    <!-- <link rel="stylesheet" href="<?php echo URL; ?>Views/template/css/bootstrap.min.css"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- <link rel="stylesheet" href="../4/litera/bootstrap.css" media="screen"> -->
-    <!-- <link rel="stylesheet" href="../_assets/css/custom.min.css"> -->
-  <!-- <script id="_carbonads_projs" type="text/javascript" src="https://srv.carbonads.net/ads/CKYIE23N.json?segment=placement:bootswatchcom&amp;callback=_carbonads_go"></script> -->
 </head>
 <body onload = "JavaScript:AutoRefresh(3000);">
 <!--body-->
   <div class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-        <a href="<?php echo URL;?>" class="navbar-brand">Monitor de colas</a>
-        <!--button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button-->        
+        <a href="<?php echo URL;?>" class="navbar-brand">Monitor de colas</a>       
           <ul class="navbar-nav">
 			<div class="container" >
 				<a class="btn btn-outline-secondary btn-sm" href="<?php echo URL;?>" role="button">TODAS</a>			  
 <?php
 foreach($listado as $cola) {
 ?>
-				<a class="btn btn-outline-secondary btn-sm" href="<?php echo URL . 'colas/ver/' . $cola;?>" role="button"><?php echo $cola?></a>
-				<!--div class="dropdown-divider"></div-->
+				<a class="btn btn-outline-secondary btn-sm" href="<?php echo URL . 'colas/ver/' . $cola;?>" role="button"><?php echo $cola?></a>				
 <?php
 }
 ?>
 			</div>
-          </ul>
-
-          <!--ul class="nav navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#" target="_blank">Link Externo</a>
-            </li>
-          </ul-->
-        
+          </ul>   
       </div>
     </div>
-<!--div class="container" id="refrescar"-->
 <div class="container">
 <?php
   }
@@ -70,37 +54,11 @@ foreach($listado as $cola) {
 <footer id="footer">
   <div class="row">
     <div class="col-lg-12">
-      <p>Página de pruebas</p>
+      <p>Desarrollado por: Eliécer Tatés - eliecer.tates@ridetel.com</p>
     </div>
 </div>
 </div>
 </footer>
-
-<!--footer class="footer">
-      <div class="container">
-        <span class="text-muted">Place sticky footer content here.</span>
-      </div>
-</footer-->
-
-<!--footer id="footer">
-        <div class="row">
-          <div class="col-lg-12">
-            <ul class="list-unstyled">
-              <li class="float-lg-right"><a href="#top">Back to top</a></li>
-              <li><a href="https://blog.bootswatch.com" onclick="pageTracker._link(this.href); return false;">Blog</a></li>
-              <li><a href="https://blog.bootswatch.com/rss/">RSS</a></li>
-              <li><a href="https://twitter.com/bootswatch">Twitter</a></li>
-              <li><a href="https://github.com/thomaspark/bootswatch/">GitHub</a></li>
-              <li><a href="../help/#api">API</a></li>
-              <li><a href="../help/#donate">Donate</a></li>
-            </ul>
-            <p>Made by <a href="https://thomaspark.co">Thomas Park</a>.</p>
-            <p>Code released under the <a href="https://github.com/thomaspark/bootswatch/blob/master/LICENSE">MIT License</a>.</p>
-            <p>Based on <a href="https://getbootstrap.com" rel="nofollow">Bootstrap</a>. Icons from <a href="https://fontawesome.com/" rel="nofollow">Font Awesome</a>. Web fonts from <a href="https://fonts.google.com/" rel="nofollow">Google</a>.</p>
-
-          </div>
-        </div>
-</footer-->
 </div>
 <script src="https://bootswatch.com/_vendor/jquery/dist/jquery.min.js"></script>
 <script src="https://bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.min.js"></script>
